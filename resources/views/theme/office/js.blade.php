af530c3cb1827e22df910cfb6e85299ac89a6bd6
Samuel Jefri Saputra Siahaan <91043243+EzByZombieS@users.noreply.github.com>
2022-07-06 14:49:40 +0700

    first commit

---
+++ resources/views/theme/office/js.blade.php
@@ -0,0 +1,23 @@
+
+
+<script>var hostUrl = "{{ asset('metronic/') }}";</script>
+<script src="{{ asset('metronic/plugins/global/plugins.bundle.js') }}"></script>
+<script src="{{ asset('metronic/js/scripts.bundle.js') }}"></script>
+<script src="{{ asset('metronic/plugins/custom/datatables/datatables.bundle.js') }}"></script>
+<script src="{{ asset('metronic/js/custom/apps/ecommerce/catalog/products.js') }}"></script>
+<script src="{{ asset('metronic/js/widgets.bundle.js') }}"></script>
+<script src="{{ asset('metronic/js/custom/widgets.js') }}"></script>
+<script src="{{ asset('metronic/js/custom/apps/chat/chat.js') }}"></script>
+<script src="{{ asset('metronic/js/custom/utilities/modals/upgrade-plan.js') }}"></script>
+<script src="{{ asset('metronic/js/custom/utilities/modals/create-app.js') }}"></script>
+<script src="{{ asset('metronic/js/custom/utilities/modals/users-search.js') }}"></script>
+<script src="{{ asset('metronic/plugins/custom/fullcalendar/fullcalendar.bundle.js') }}"></script>
+<script src="{{ asset('metronic/plugins/custom/tinymce/tinymce.bundle.js') }}"></script>
+
+<script src="{{asset('js/auth.js')}}"></script>
+<script src="{{asset('js/toastr.js')}}"></script>
+<script src="{{asset('js/plugin.js')}}"></script>
+<script src="{{asset('js/method.js')}}"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-confirm/3.3.2/jquery-confirm.min.js"></script>
+
+
